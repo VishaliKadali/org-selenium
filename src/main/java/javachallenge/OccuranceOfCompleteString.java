955c6e841fbf655761db9577cc3ea3c4beae2ab0
unknown <kvishalicse@gmail.com>
2024-08-11 21:58:30 +0530

    added OccuranceOfCompleteString

---
+++ src/main/java/javachallenge/OccuranceOfCompleteString.java
@@ -0,0 +1,32 @@
+package javachallenge;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class OccuranceOfCompleteString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String text = "we learn java basics as part of java sessions in java week1";
+		String[] words=text.split("\\s");
+		
+		HashMap<String,Integer> hmap=new HashMap<String,Integer>();
+		
+		for (String eachWord : words) {
+			if(hmap.containsKey(eachWord)) {
+				int count=hmap.get(eachWord)+1;
+				hmap.put(eachWord, count);
+			}
+			else {
+				hmap.put(eachWord, 1);
+			}
+		}
+		
+		for (Entry<String, Integer> eachEntry : hmap.entrySet()) {
+			if(eachEntry.getValue()>1) {
+				System.out.println("Word : " +eachEntry.getKey()+ " is present : " +eachEntry.getValue()+ " times");
+			}
+		}
+	}
+
+}
